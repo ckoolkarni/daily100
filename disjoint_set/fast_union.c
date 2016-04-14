5cee2abecb6cf30535ba4457215cef8cdcda4072
ckoolkarni <chaitanyakulkarni15@gmail.com>
2016-04-13 18:05:32 -0700

    -Implementation of rank based algoritm for Union Find.

--- disjoint_set/fast_union.c
+++ disjoint_set/fast_union.c
@@ -5,7 +5,7 @@
 
 struct set_node {
 	int data;
-	int child_count;
+	int rank;
 	struct set_node *parent;
 };
 
@@ -14,7 +14,7 @@ alloc_mem(size_t size)
 {
     void *p;
 
-	if ((p = malloc(size)) == NULL) {
+	if ((p = (void *) malloc(size)) == NULL) {
 		perror("malloc : ");
 		exit(EXIT_FAILURE);
 	}
@@ -26,9 +26,10 @@ alloc_set_node(int data)
 {
 	struct set_node *n;
 
-	n = alloc_mem(sizeof(struct set_node));
+	n = (struct set_node *)alloc_mem(sizeof(struct set_node));
 	n->data = data;
-	n->child_count = 0;
+	n->rank = 0;
+	n->parent = NULL;
 	return n;
 }
 
@@ -45,10 +46,10 @@ print_set(struct set_node **set, int n)
 	int i;
 
 	printf("\n%16s %10s %10s %10s\n",
-			"ADDRESS", "DATA", "CHILD_COUNT", "PARENT");
+			"ADDRESS", "DATA", "rank", "PARENT");
 	for (i = 0; i < n; i++) {
 		printf("%16p %10d %10d %10d\n", 
-				set[i], set[i]->data, set[i]->child_count, 
+				set[i], set[i]->data, set[i]->rank, 
 				set[i]->parent == NULL ? -1 : set[i]->parent->data);
 	}
 }
@@ -59,48 +60,14 @@ init_set(int n)
 	int i;
 	struct set_node **set;
 
-	set = alloc_mem(sizeof(struct set_node**));
+	set = alloc_mem(n * sizeof(struct set_node **));
 
-	printf("\n%16s %10s %10s %10s\n",
-			"ADDRESS", "DATA", "CHILD_COUNT", "PARENT");
 	for (i = 0; i < n; i++) {
-		set[i] = alloc_mem(sizeof(struct set_node *));
-		set[i]->data = i;
-		set[i]->child_count = 0;
-		set[i]->parent = NULL;
-
-		printf("%16p %10d %10d %10d\n", 
-				set[i], set[i]->data, set[i]->child_count, 
-				set[i]->parent == NULL ? -1 : set[i]->parent->data);
+	    set[i] = alloc_set_node(i);
 	}
+
+	print_set(set, n);
 	return set;
-}
-
-void
-make_union(struct set_node **set, int a, int b)
-{
-	assert(set != NULL);
-	
-	/* in case of same number of children use data to choose the parent */
-	printf("%s \n", set[a] == NULL ? "NULL" : "NOT NULL");
-	printf("%d \n", set[b]->child_count);
-	if (set[a]->child_count == set[b]->child_count) {
-		if (set[a]->data > set[b]->data) {
-			set[b]->parent = set[a];
-			set[a]->child_count++;
-		} else {
-			set[a]->parent = set[b];
-			set[b]->child_count++;
-		}
-	} else {
-		if (set[a]->child_count > set[b]->child_count) {
-			set[b]->parent = set[a];	
-			set[a]->child_count++;
-		} else {
-			set[a]->parent = set[b];	
-			set[b]->child_count++;
-		}
-	}
 }
 
 struct set_node * 
@@ -121,11 +88,33 @@ find_set(struct set_node **set, struct set_node *n)
 }
 
 void
+make_union(struct set_node **set, int a, int b)
+{
+	struct set_node *aroot;
+	struct set_node *broot;
+
+	assert(set != NULL);
+
+    aroot = find_set(set, set[a]);
+    broot = find_set(set, set[b]);
+
+	if (aroot->rank < broot->rank) {
+		aroot->parent = broot;	
+	} else if (aroot->rank > broot->rank) {
+		broot->parent = aroot;	
+	} else {
+		broot->parent = aroot;
+		aroot->rank++;
+	}
+}
+
+void
 free_set(struct set_node **set, int n)
 {
 	int i;
 
 	assert(set != NULL);
+
 	for (i = 0; i < n; i++) {
 		free_mem(set[i]);
 	}
@@ -143,21 +132,16 @@ int main(int argc, char *argv[])
 	set = init_set(n);
 
 	print_set(set, n);
-#if 0
     make_union(set, 0, 1);
-	print_set(set, n);
     make_union(set, 2, 3);
-	print_set(set, n);
     make_union(set, 3, 4);
-	print_set(set, n);
     make_union(set, 8, 9);
-	print_set(set, n);
     make_union(set, 4, 7);
-#endif
 	for (i = 0; i < n; i++) {
 		t = find_set(set, set[i]);
 		printf("set %d element %d\n", t->data, i);
 	}
+
 	free_set(set, n);
 	return 0;
 }
